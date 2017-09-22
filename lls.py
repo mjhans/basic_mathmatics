6b320b6c3f9a7ac1e9a1005fbdce2ce9c7f6b37a
mjhans <mjhans83@interpark.com>
2017-09-22 17:40:20 +0900

    lls 함수 사용

--- lls.py
+++ lls.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 import multiprocessing as mp
 import numpy as np
+import json
 
 
 from matrix import Matrix
@@ -17,27 +18,76 @@ def file_open():
         for row in readCSV:
             Xm = list()
             Xm.append(1)
-            Xm.append(float(row[0]))
+            Xm.append(int(row[1]))
             Xmn.append(Xm)
 
-            Ym1.append([float(row[1])])
+            Ym1.append([int(row[0])])
 
     return (Xmn, Ym1,)
 
 if __name__ == "__main__":
+    raws = None
+
+    with open("input.json","r") as fp:
+        raws = json.load(fp)
+
+    print(raws)
     X, Y = file_open()
-    np_X = np.array(X)
-    np_XT = np_X.T
-    np_Y = np.array(Y)
+    input_data = dict()
+    input_data["X"] = X
+    input_data["Y"] = Y
 
-    print(np_X.shape, np_Y.shape, np_X.T.shape)
-    XtX = np_X.T.dot(np_X)
-    print(XtX)
+    with open("input.json","w") as fp:
+        json.dump(input_data, fp)
+
+    np_X = np.array(X, dtype=np.float32)   
+    np_XT = np.transpose(np_X)#np_X.T
+    np_XT.astype(np.float32)    
+    np_Y = np.array(Y, dtype=np.float32)
 
 
-    mat_X = Matrix(X)
-    mat_Y = Matrix(Y)
+    print(np_XT.shape, np_X.shape, np_Y.shape)
+    XtX_1 = np_XT.dot(np_X)
+    #XtX_1.astype(np.int64)
+    
+    try:        
+        I_XtX = np.linalg.inv(XtX_1)
+    except numpy.linalg.LinAlgError as err:
+    # Not invertible. Skip this one.
+        print("{}".format(err))
+        pass       
+    
+    #print("Inverse matrix:{}".format(I_XtX.tolist()))    
+    with open("inverse_result_2.csv","w") as fp:
+        imat = I_XtX.tolist()
+        for mat in imat:
+            for m in mat:
+                fp.write("{}\n".format(m))
 
-    print("{}, {}, {}".format(mat_X, mat_Y, mat_X.T()))
-    XtX = mat_X.T().dot(mat_X)
-    print(XtX.ELEM)
+    # get beta hat
+    # beta_hat = inverse(Xt X) * Xt*Y
+    XtY = np_XT.dot(np_Y)
+    beta_hat = I_XtX.dot(XtY)
+    #beta_hat = I_XtX.dot(np_XT).dot(np_Y)
+    #print("Beta hat:{}".format(beta_hat.tolist()))
+    with open("betahat_result_2.csv","w") as fp:
+        beta_list = beta_hat.tolist()
+        for bhat in beta_list:
+            for bh in bhat:
+                fp.write("{}\n".format(bh))
+
+    # get Y hat
+    # Y_hat = X * beta_hat
+    Y_hat = np_X.dot(beta_hat)
+    #print("Y_hat:{}".format(Y_hat))
+
+    # 파일쓰기
+    with open("Yhat_result1.csv","w") as fp:
+        Y_hat_list = Y_hat.tolist()
+        for Yhat in Y_hat_list:
+            for Yh in Yhat:
+                fp.write("{}\n".format(Yh))
+
+    # get ABS(Y_hat - Y)
+
+
